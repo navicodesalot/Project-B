769ec7a02a70bd70c8d523368035cccb30680705
navi <melkissappig@gmail.com>
2025-10-07 16:23:37 +0200

    Update BookModel.cs

--- DataModels/BookModel.cs
+++ DataModels/BookModel.cs
@@ -1,9 +1,12 @@
 ﻿public class BookModel
 {
-    public string Id { get; set; } // Changed from Int64 to string
+    public string Id { get; set; }
     public string Title { get; set; }
     public string Genre { get; set; }
     public int AuthorId { get; set; }
+
+    // should make dapper not be mean
+    public BookModel() { }
 
     public BookModel(string id, string title, string genre, int authorId)
     {
